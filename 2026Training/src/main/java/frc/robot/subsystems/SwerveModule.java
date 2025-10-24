68049eaeaab56fbe0d6ed97aaf6274001a4a43c0
1212 <mjgarve1@gmail.com>
2025-10-24 08:37:22 -0700

    Updating to latest frc build tools

--- 2026Training/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ 2026Training/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,7 +11,6 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.controller.PIDController;
